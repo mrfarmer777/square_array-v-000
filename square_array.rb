666a9e5e0655f74f2086386e80e9a3e700339e73
Matt Farmer <mrfarmer777@gmail.com>
2018-05-26 04:27:11 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,14 @@
 def square_array(array)
   # your code here
+  newArray=[]
+  array.each do |num|
+    newArray.push(num**2)
+  end
+  return newArray
 end
+
+def collect_trial(array)
+  newArray=array.collect {|num| num**2}
+end
+
+collect_trial([1,4,67,78])
